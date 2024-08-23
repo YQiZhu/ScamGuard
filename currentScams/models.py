d1063f989d366f03d4efda9b8247140a82405b86
Ubuntu <ubuntu@ip-172-31-45-225.ec2.internal>
2024-08-23 02:26:05 +0000

    Setup data model

--- currentScams/models.py
+++ currentScams/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class ScamReport(models.Model):
-    start_of_month = models.DateField()  # StartOfMonth
+    start_of_month = models.CharField(max_length=100)  # StartOfMonth
     amount_lost = models.DecimalField(max_digits=10, decimal_places=2)  # Amount_lost
     number_of_reports = models.IntegerField()  # Number_of_reports
     average_report = models.DecimalField(max_digits=10, decimal_places=2)  # Average_report
@@ -12,3 +12,6 @@ class ScamReport(models.Model):
     category_id = models.IntegerField()  # Category_ID
     complainant_id = models.IntegerField()  # Complainant_ID
     report_id = models.AutoField(primary_key=True)  # Report_ID (AutoField will auto-increment)
+
+    class Meta:
+        db_table = 'scam_report'
--- currentScams/views.py
+++ currentScams/views.py
@@ -4,6 +4,10 @@ from django.http import JsonResponse
 import requests
 from django.conf import settings
 from rest_framework import viewsets
-from .models import ReportScam
+from .models import ScamReport
 
 # Create your views here.
+
+@require_GET
+def get_report(request):
+    report = ScamReport.objects.all()
