3b97b112661bd3bbb9024cd3325d7293e9cf76cf
Yanqi Zhu <yzhu0104@student.monash.edu>
2024-08-27 14:53:58 +1000

    Update view

--- currentScams/urls.py
+++ currentScams/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from .views import get_report
+from .views import get_most_frequent_scams, get_scams_highest_loss, get_most_scams_contact_methods
 
 urlpatterns = [
-    path('api/scam-report/', get_report, name='scam_report'),
+    path('api/most_frequent_scams/', get_most_frequent_scams, name='most_frequent_scams'),
+    path('api/scams_highest_loss/', get_scams_highest_loss, name='scams_highest_loss'),
+    path('api/most_scams_contact_methods/', get_most_scams_contact_methods, name='most_scams_contact_methods'),
+    
 ]
--- currentScams/views.py
+++ currentScams/views.py
@@ -5,6 +5,7 @@ import requests
 from django.conf import settings
 from rest_framework import viewsets
 from .models import ScamReport, ScamCategory, Complainant
+import pandas as pd
 
 # Create your views here.
 # Step 1: Query the data from PostgreSQL using Django ORM
