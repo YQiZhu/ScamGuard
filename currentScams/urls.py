f0d9891f2c29b4fea033f48ec8e6c78c993357d6
Yanqi Zhu <yzhu0104@student.monash.edu>
2024-08-27 23:27:51 +1000

    Update urls in django

--- currentScams/urls.py
+++ currentScams/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('api/most_frequent_scams/', get_most_frequent_scams, name='most_frequent_scams'),
     path('api/scams_highest_loss/', get_scams_highest_loss, name='scams_highest_loss'),
     path('api/most_scams_contact_methods/', get_most_scams_contact_methods, name='most_scams_contact_methods'),
-    
 ]
