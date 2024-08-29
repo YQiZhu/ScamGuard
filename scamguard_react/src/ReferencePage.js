5b98d87ba00e386041f2841f123b58cef73cb2c3
angusmcnicol <mcnicolangus@gmail.com>
2024-08-29 19:59:59 +1000

    Added references

--- scamguard_react/src/ReferencePage.js
+++ scamguard_react/src/ReferencePage.js
@@ -7,9 +7,36 @@ function ReferencePage() {
       <h1>References</h1>
       <p>Here is the list of references used in the website.</p>
       <ul>
-        <li><a href="https://example.com">Reference 1</a></li>
-        <li><a href="https://example.com">Reference 2</a></li>
-        <li><a href="https://example.com">Reference 3</a></li>
+        <li><a href="https://www.scamwatch.gov.au/types-of-scams/text-or-sms-scams">Scamwatch Text or SMS Scams </a></li>
+        <li><a href="https://www.nab.com.au/about-us/security/online-safety-tips/identify-spam-phishing-messages">NAB Identify Spam Phishing Messages </a></li>
+        <li><a href="https://consumer.ftc.gov/articles/how-recognize-and-report-spam-text-messages">FTC How to Recognize and Report Spam Text Messages</a></li>
+        <li><a href="https://www.scamwatch.gov.au/types-of-scams/phone-scams">Scamwatch Phone Scams</a></li>
+        <li><a href="https://www.telstra.com.au/cyber-security-and-safety/scams/scam-phone-calls">Telstra Scam Phone Calls</a></li>
+        <li><a href="https://www.telstra.com.au/exchange/five-ways-to-spot-a-scam-call">Telstra Five Ways to Spot a Scam Call</a></li>
+        <li><a href="https://www.scamwatch.gov.au/types-of-scams/email-scams">Scamwatch Email Scams</a></li>
+        <li><a href="https://support.microsoft.com/en-au/windows/protect-yourself-from-phishing-0c7ea947-ba98-3bd9-7184-430e1f860a44">Microsoft Protect yourself from phishing</a></li>
+        <li><a href="https://health.ucdavis.edu/cybersecurity/learning-center/spot-phishing-messages">UC Davis Spot Phishing Messages</a></li>
+        <li><a href="https://www.scamwatch.gov.au/types-of-scams/social-media-scams">Scamwatch Social Media Scams</a></li>
+        <li><a href="https://cyberseniors.org/uncategorized/how-to-spot-and-avoid-social-media-scams/">Cyber Seniors How to Spot and Avoid Social Media Scams</a></li>
+        <li><a href="https://www.scamwatch.gov.au/types-of-scams/website-scams">Scamwatch Website Scams</a></li>
+        <li><a href="https://www.choice.com.au/shopping/online-shopping/buying-online/articles/how-to-spot-a-fake-fraudulent-or-scam-website">Choice 6 ways to spot a scam shopping site</a></li>
+        <li><a href="https://www.aura.com/learn/how-to-identify-fake-websites">Aura How to Identify Fake Websites</a></li>
+        <li><a href="https://www.scamwatch.gov.au/types-of-scams/online-dating-and-romance-scams">Scamwatch Online Dating and Romance Scams</a></li>
+        <li><a href="https://www.commbank.com.au/support/security/romance-scams.html">CommBank Romance Scams</a></li>
+        <li><a href="https://www.nab.com.au/about-us/security/online-safety-tips/romance-scams">NAB Romance Scams</a></li>
+        <li><a href="https://www.aura.com/learn/how-to-spot-a-scammer-on-a-dating-site">Aura How to Spot a Scammer on a Dating Site</a></li>
+        <li><a href="https://www.scamwatch.gov.au/types-of-scams/investment-scams">Scamwatch Investment Scams</a></li>
+        <li><a href="https://moneysmart.gov.au/financial-scams/investment-scams">Moneysmart Investment Scams</a></li>
+        <li><a href="https://www.commbank.com.au/support/security/investment-scams.html">CommBank Investment Scams</a></li>
+        <li><a href="https://www.macquarie.com.au/security-and-fraud/scams/how-an-investment-scammer-can-find-and-target-you.html">Macquarie How an investment scammer can find and target you</a></li>
+        <li><a href="https://www.scamwatch.gov.au/types-of-scams/product-and-service-scams">Scamwatch Product and Service Scams</a></li>
+        <li><a href="https://www.nab.com.au/about-us/security/online-safety-tips/buying-selling-scams">NAB Buying and Selling Scams</a></li>
+        <li><a href="https://www.scamwatch.gov.au/types-of-scams/jobs-and-employment-scams">Scamwatch Job Scams</a></li>
+        <li><a href="https://www.aura.com/learn/how-to-identify-job-scams">Aura How to Identify Job Scams</a></li>
+        <li><a href="https://www.nab.com.au/about-us/security/online-safety-tips/employment-scams">NAB Employment Scams</a></li>
+        <li><a href="https://www.scamwatch.gov.au/types-of-scams/impersonation-scams">Scamwatch Impersonation Scams</a></li>
+        <li><a href="https://www.newcastlepermanent.com.au/hub/impersonation-scams">Newcastle Permanent Impersonation Scams</a></li>
+        <li><a href="https://www.telstra.com.au/cyber-security-and-safety/active-scams#phone">Telstra Active Scams</a></li>
       </ul>
     </div>
   );
