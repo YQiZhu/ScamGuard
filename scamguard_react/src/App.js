c15f2d5a2aba72242deb83f1c183e0d8a5466183
Yanqi Zhu <111552343+YQiZhu@users.noreply.github.com>
2024-09-06 11:38:06 +1000

    Update App.js

--- scamguard_react/src/App.js
+++ scamguard_react/src/App.js
@@ -53,8 +53,6 @@ function App() {
                     src={require('./images/think.png')}
                     alt="think"
                   />
-                  <p>There are XXX many of people been scammed in last year, where age between xx to xx
-                    senior become the most vulnerable group when facing the scams.</p>
                   <p>Stay informed on current scams, learn about different scams and find out how to protect yourself from scammers.</p>
                   <p><strong>To start your journey on our website, go back to the top and click on the navigation bars.</strong></p>
                   <button onClick={scrollToTop}>Go to Top of the Page</button>
