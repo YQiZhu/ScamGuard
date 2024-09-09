7498b56a39f40e9a9f59429f75567eff906fc50e
user <yzhu0104@student.monash.edu>
2024-09-09 22:32:10 +1000

    Update risk assessment chart

--- scamguard_react/src/CurrentScams/PopularContactMethods.js
+++ scamguard_react/src/CurrentScams/PopularContactMethods.js
@@ -47,7 +47,7 @@ function PopularContactMethods() {
                     labelFormatter={(label) => `Scam Type: ${label}`}
                 />
                 <Legend formatter={(value) => "Number of Reports"} />
-                <Bar dataKey="number_of_reports" fill="#8884d8" />
+                <Bar dataKey="number_of_reports" fill="#8884d8" radius={[30, 30, 30, 30]}/>
             </BarChart>
             <p>The <strong>Email</strong> ranked at the <strong>first place</strong> in the top three most frequent methods scammers used to contact Australians from the age group 65 and over.</p>
 
