b2fe5f9bb5aae5e251f9c5cacb4061071ea00823
Yanqi Zhu <yzhu0104@student.monash.edu>
2024-09-10 15:49:18 +1000

    Update merge

--- scamguard_react/src/App.js
+++ scamguard_react/src/App.js
@@ -60,7 +60,7 @@ function App() {
                 >
                   <ShaderGradient
                     control="query"
-                    urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=2.5&cAzimuthAngle=180&cDistance=2.8&cPolarAngle=80&cameraZoom=15&color1=%23003fff&color2=%23585ed6&color3=%235f3ed7&destination=onCanvas&embedMode=off&envPreset=dawn&format=gif&fov=50&frameRate=10&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=2.1&positionX=0&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=60&rotationY=0&rotationZ=-60&shader=defaults&type=sphere&uAmplitude=3.8&uDensity=3.3&uFrequency=0&uSpeed=0.01&uStrength=2&uTime=8&wireframe=false&zoomOut=false"
+                    urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=2.5&cAzimuthAngle=70&cDistance=2.8&cPolarAngle=100&cameraZoom=25&color1=%23003fff&color2=%23585ed6&color3=%235f3ed7&destination=onCanvas&embedMode=off&envPreset=dawn&format=gif&fov=30&frameRate=10&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1.5&positionX=0&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=60&rotationY=0&rotationZ=-60&shader=defaults&type=sphere&uAmplitude=0&uDensity=4.3&uFrequency=0&uSpeed=0.01&uStrength=1.1&uTime=8&wireframe=false&zoomOut=false"
                   />
                 </ShaderGradientCanvas>
 
