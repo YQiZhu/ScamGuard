cb250e8afb2df17d850fb8f6f6fd7198e199afe5
user <yzhu0104@student.monash.edu>
2024-09-16 00:06:20 +1000

    Remove useless folders and files

--- ScamGuard/settings.py
+++ ScamGuard/settings.py
@@ -164,9 +164,3 @@ LOGGING = {
     },
 }
 
-
-# Celery configuration
-CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Redis URL
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'  # To store results of tasks (optional)
