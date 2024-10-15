0244609780bbcf54d3fe87f6dd96aa7703b11a98
Yanqi Zhu <yzhu0104@student.monash.edu>
2024-10-15 11:57:37 +1100

    Update backend code

--- model_interface/views.py
+++ model_interface/views.py
@@ -114,6 +114,9 @@ def predict_value(request, model_type):
             processed_input = text_vectorizer.transform([clean_input])
             print(f"Processed Input Shape: {processed_input.shape}")
             print(f"Processed Input Type: {type(processed_input)}")
+            if isinstance(processed_input, csr_matrix):
+                processed_input = processed_input.toarray()  # Convert to dense array
+
             try:
                 prediction = model_message.predict(processed_input)[0]
                 prob = model_message.predict_proba(processed_input)[0][prediction]
